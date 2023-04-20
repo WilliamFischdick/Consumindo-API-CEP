171b8c96acbcfba06ed44772fc346cdc96f729c6
William Fischdick <jwfbit@hotmail.com>
2023-04-20 18:48:22 -0300

    :construction: feat: Construindo a lógica para requisição dos dados da API.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 
-var consultaCEP = fetch('http://viacep.com.br/ws/01001000/json/');
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error("CEP não existente!");
+        }
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
