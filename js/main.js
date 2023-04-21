881f1005d5e814a6a3ac52edeec62b2bd7f0ad6d
William Fischdick <jwfbit@hotmail.com>
2023-04-21 20:09:56 -0300

    :construction: feat: Finalizando o tratamento das informações e adicionando mensagem de erro.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
-            throw Error("CEP não existente!");
+            throw Error('CEP não existente!');
         }
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
         return consultaCEPConvertida;
     } catch (erro) {
         console.log(erro);
+        mensagemErro.innerHTML = `<p>CEP Inválido</p>`;
     }
 }
+
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout",() => buscaEndereco(cep.value));
+
 
